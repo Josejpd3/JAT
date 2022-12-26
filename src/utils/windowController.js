718d046ecb592c943f11d66543c76d61288ec58b
Jose Plasencia <josejpd3@gmail.com>
2022-12-25 23:23:46 -0500

    showHideMenus addEventlistener toggle(src/utils/windowController)

--- src/utils/windowController.js
+++ src/utils/windowController.js
@@ -19,3 +19,17 @@ maxResBtn.addEventListener('click', ()=> {
 closeBtn.addEventListener('click', ()=> {
     ipc.send('closeApp')
 })
+
+
+
+showHideMenus.addEventListener('click', ()=> {
+    if(!isLeftMenuActive) {
+        mySidebar.style.width = '0px'
+        isLeftMenuActive = true
+        console.log(isLeftMenuActive);
+    } else {
+        mySidebar.style.width = '220px'
+        isLeftMenuActive = false
+        console.log(isLeftMenuActive);
+    }
+})
