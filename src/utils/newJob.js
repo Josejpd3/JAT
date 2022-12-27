9c06d5dea95a4080038d2f5a60e60d8a49a31be0
Jose Plasencia <josejpd3@gmail.com>
2022-12-27 14:35:23 -0500

    determine counterId by the jobs.length quantity (src/utils/newJob.js)

--- src/utils/newJob.js
+++ src/utils/newJob.js
@@ -1,3 +1,6 @@
 const fs = require('fs');
 var data = fs.readFileSync('C:/Users/14072/Desktop/Projects/REPOS/Job-Application-Tracker-v2/src/data/jobs.json');
 var jobs = JSON.parse(data);
+
+let counter = jobs.length
+
