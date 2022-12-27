8888e64520fd99bc4acbcba0cf05837d5fbe2126
Jose Plasencia <josejpd3@gmail.com>
2022-12-27 13:39:04 -0500

    fetch jobs.json data (src/utils/getJobs.js)

--- src/utils/getJobs.js
+++ src/utils/getJobs.js
@@ -3,3 +3,14 @@ let data = fs.readFileSync(__dirname + '/data/jobs.json');
 let jobs = JSON.parse(data);
 
 
+// Get all jobs data
+fetch(__dirname + "/data/jobs.json")
+.then(function(response) {
+    return response.json();
+})
+.then(function(jobs){
+
+
+
+
+    });
