981c80813bd6582b6c17ec69380f01d56d7294fc
Jose Plasencia <josejpd3@gmail.com>
2022-12-28 13:39:53 -0500

    changed quotes and added semicolons (src/utils/newJob.js)

--- src/utils/newJob.js
+++ src/utils/newJob.js
@@ -2,19 +2,20 @@ const fs = require('fs');
 var data = fs.readFileSync('C:/Users/14072/Desktop/Projects/REPOS/Job-Application-Tracker-v2/src/data/jobs.json');
 var jobs = JSON.parse(data);
 
-let counter = jobs.length
+let counter = jobs.length;
 
-compName = document.getElementById('compName')
-place = document.getElementById('place')
-position = document.getElementById('position')
-estSalary = document.getElementById('estSalary')
-stat = document.getElementById('stat')
-dateApplied = document.getElementById('dateApplied')
-contact = document.getElementById('contact')
-fileInput = document.getElementById('fileInput')
+compName = document.getElementById("compName");
+place = document.getElementById("place");
+position = document.getElementById("position");
+estSalary = document.getElementById("estSalary");
+stat = document.getElementById("options");
+dateApplied = document.getElementById("dateApplied");
+contact = document.getElementById("contact");
+imgInput = document.getElementById("imgInput");
+resumeInput = document.getElementById("resumeInput");
 
-saveJob = document.getElementById('saveJob')
-jsonText = document.getElementById('jsontext')
+saveJob = document.getElementById("saveJob");
+jsonText = document.getElementById("jsontext");
 
 saveJob.addEventListener("click", function(){
     const file = fileInput.files[0]
