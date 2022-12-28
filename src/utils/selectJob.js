8431be4e54c8bbe59c6dce64a204d397f74ea19d
Jose Plasencia <josejpd3@gmail.com>
2022-12-28 14:28:30 -0500

    jobPosting and resume directory path (src/utils/selectJob.js)

--- src/utils/selectJob.js
+++ src/utils/selectJob.js
@@ -1,2 +1,5 @@
 const fs = require('fs')
 
+const jobPostingsPath = "C:/Users/14072/Desktop/Projects/REPOS/Job-Application-Tracker-v2/src/data/jobPostings/"
+const resumePath = "C:/Users/14072/Desktop/Projects/REPOS/Job-Application-Tracker-v2/src/data/resumes/"
+
