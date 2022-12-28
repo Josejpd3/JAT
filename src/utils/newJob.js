71cb3a655a3b5a0f88f4bc7c7ead6665b3f152d0
Jose Plasencia <josejpd3@gmail.com>
2022-12-28 13:40:37 -0500

    restructured eventListener and added if statements for imgs and files (src/utils/newJob.js)

--- src/utils/newJob.js
+++ src/utils/newJob.js
@@ -17,20 +17,46 @@ resumeInput = document.getElementById("resumeInput");
 saveJob = document.getElementById("saveJob");
 jsonText = document.getElementById("jsontext");
 
-saveJob.addEventListener("click", function(){
-    const file = fileInput.files[0]
-    const savePath = "C:/Users/14072/Desktop/Projects/REPOS/Job-Application-Tracker-v2/src/data/jobs.json";
-    const destination = "C:/Users/14072/Desktop/Projects/REPOS/Job-Application-Tracker-v2/src/icons/" + file.name;
-    
-    jobs.push({"counterId": counter,"compName": compName.value, "place": place.value, "position": position.value, "estSalary": estSalary.value, "stat": stat.value, "dateApplied": dateApplied.value, "contact": contact.value, "img": file.name});
-    var json = JSON.stringify(jobs, null, 2);
-    
-    fs.writeFile(savePath, json, 'utf8', finished)
-    function finished(err) {
-        console.log("finished")
-    }
-    const source = fs.createReadStream(file.path)
-    const destinationStream = fs.createWriteStream(destination)
-    source.pipe(destinationStream)
+saveJob.addEventListener("click", function() {
+  const savePath = "C:/Users/14072/Desktop/Projects/REPOS/Job-Application-Tracker-v2/src/data/jobs.json";
+  const imgFile = imgInput.files[0];
+  const resumeFile = resumeInput.files[0];
 
-})
+  jobs.push({
+    counterId: counter,
+    compName: compName.value,
+    place: place.value,
+    position: position.value,
+    estSalary: estSalary.value,
+    stat: stat.value,
+    dateApplied: dateApplied.value,
+    contact: contact.value,
+    img: '',
+    resume: '',
+  });
+  
+  if (imgInput.value) {
+    jobs.img = imgFile.name;
+    const imgDestination = "C:/Users/14072/Desktop/Projects/REPOS/Job-Application-Tracker-v2/src/data/jobPostings/" + imgFile.name;
+    const imgSource = fs.createReadStream(imgFile.path);
+    const imgDestinationStream = fs.createWriteStream(imgDestination);
+    imgSource.pipe(imgDestinationStream);
+  }
+  if (resumeInput.value) {
+    jobs.resume = resumeFile.name;
+    const resumeDestination = "C:/Users/14072/Desktop/Projects/REPOS/Job-Application-Tracker-v2/src/data/resumes/" + resumeFile.name;
+    const resumeSource = fs.createReadStream(resumeFile.path);
+    const resumeDestinationStream = fs.createWriteStream(resumeDestination);
+    resumeSource.pipe(resumeDestinationStream);
+  }
+
+
+
+  var json = JSON.stringify(jobs, null, 2);
+
+  fs.writeFile(savePath, json, "utf8", finished);
+  function finished(err) {
+    console.log("finished");
+  }
+
+});
