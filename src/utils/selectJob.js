7a8bec7b62e4165e09d2588de79cea4db33a723f
Jose Plasencia <josejpd3@gmail.com>
2022-12-28 15:03:38 -0500

    determine counterId of selected job (src/utils/selectJob.js)

--- src/utils/selectJob.js
+++ src/utils/selectJob.js
@@ -1,5 +1,14 @@
 const fs = require('fs')
 
-const jobPostingsPath = "C:/Users/14072/Desktop/Projects/REPOS/Job-Application-Tracker-v2/src/data/jobPostings/"
-const resumePath = "C:/Users/14072/Desktop/Projects/REPOS/Job-Application-Tracker-v2/src/data/resumes/"
+const mainDataPath = "C:/Users/14072/Desktop/Projects/REPOS/Job-Application-Tracker-v2/src/data/";
+
+const jobPostingsPath = mainDataPath + "jobPostings/";
+const resumePath = mainDataPath + "resumes/";
+
+// Determine CounterId of the selected Job
+const selectedJobPath = mainDataPath + 'selectedJob.json';
+const selectedJobData = fs.readFileSync(selectedJobPath);
+let selectedJobId = JSON.parse(selectedJobData);
+
+
 
