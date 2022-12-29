642e8d71768dc720d086e9d0247d702257a00eb4
Jose Plasencia <josejpd3@gmail.com>
2022-12-29 18:49:41 -0500

    jobs, img, resume paths (src/utils/updateJob.js)

--- src/utils/updateJob.js
+++ src/utils/updateJob.js
@@ -6,3 +6,7 @@ import selectedJobId from "./selectJob.js";
 const currentFilePath = __filename;
 const mainDataPath = path.join(path.dirname(path.dirname(path.dirname(currentFilePath))) + '/data/');
 
+// Paths
+const jobsPath = path.join(mainDataPath + "jobs.json");
+const imgPath = path.join(mainDataPath + "jobPostings/")
+const resumePath = path.join(mainDataPath + "resumes/")
