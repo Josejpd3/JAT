ffe29815b225a4a4ba8fc2d9400c112fc202c9ed
Jose Plasencia <josejpd3@gmail.com>
2022-12-29 18:50:01 -0500

    select form element inputs (src/utils/updateJob.js)

--- src/utils/updateJob.js
+++ src/utils/updateJob.js
@@ -10,3 +10,16 @@ const mainDataPath = path.join(path.dirname(path.dirname(path.dirname(currentFil
 const jobsPath = path.join(mainDataPath + "jobs.json");
 const imgPath = path.join(mainDataPath + "jobPostings/")
 const resumePath = path.join(mainDataPath + "resumes/")
+
+// Selecting the every form input
+let compNameElement = document.getElementById("compName");
+let placeElement = document.getElementById("place");
+let positionElement = document.getElementById("position");
+let estSalaryElement = document.getElementById("estSalary");
+let statElement = document.getElementById("options");
+let dateAppliedElement = document.getElementById("dateApplied");
+let contactElement = document.getElementById("contact");
+let imgInputElement = document.getElementById("imgInput");
+let resumeInputElement = document.getElementById("resumeInput");
+let updateJob = document.getElementById("updateJob");
+
