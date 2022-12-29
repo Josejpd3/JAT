cc83a288fb62154e251c2acf415349f3ce63c36d
Jose Plasencia <josejpd3@gmail.com>
2022-12-29 18:48:55 -0500

    get currentFilePath and data path (src/utils/updateJob.js)

--- src/utils/updateJob.js
+++ src/utils/updateJob.js
@@ -3,3 +3,6 @@ const path = require('path');
 const { ipcRenderer } = require("electron");
 import selectedJobId from "./selectJob.js";
 
+const currentFilePath = __filename;
+const mainDataPath = path.join(path.dirname(path.dirname(path.dirname(currentFilePath))) + '/data/');
+
